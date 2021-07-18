dd8a86e109709c8324889f00cb48b2c5dff45fdf
Albin Varghese <albin8285@gmail.com>
2021-07-19 03:39:19 +0530

    include tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import './index.css'
 
 createApp(App).use(router).mount('#app')
