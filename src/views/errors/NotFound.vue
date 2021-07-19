5c941aa1bec1060b54d8c98a5139f64ae9cdd58c
Albin Varghese <albin8285@gmail.com>
2021-07-19 22:40:16 +0530

    show NotFound page if route dosent exist

---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div class="sm:text-center lg:text-left">
+          <h1 class="text-7xl font-semibold">404</h1>
+          <h3 class="my-4 text-xl">Page not found</h3>
+        </div>
+        
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
