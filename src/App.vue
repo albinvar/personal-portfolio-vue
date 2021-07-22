65761acc808ced1593af30d3b3795348ef95e39a
Albin Varghese <albin8285@gmail.com>
2021-07-22 11:41:14 +0530

    add title

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
                   <span class="sr-only">Workflow</span>
                   <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg">
                 </router-link>
+                <span class="text-xl text-gray-500 font-bold animate__animated animate__jackInTheBox animate__delay-1s ">Albin Varghese</span>
                 <div class="-mr-2 flex items-center md:hidden">
                   <button type="button" @click="toggleShowNavbar" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
