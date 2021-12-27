6e1e311831aa3830515c521384577d0c959dcf7b
Albin Varghese <albin8285@gmail.com>
2021-12-27 19:36:11 +0530

    modify Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,109 +1,14 @@
 <template>
   <div class="bg-gray-900">
-    <div class="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div class="grid row-gap-10 mb-8 lg:grid-cols-6">
-        <div class="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
-          <div>
-            <p class="font-medium tracking-wide text-gray-300">Category</p>
-            <ul class="mt-2 space-y-2">
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">News</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">World</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Games</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">References</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p class="font-medium tracking-wide text-gray-300">Apples</p>
-            <ul class="mt-2 space-y-2">
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Web</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">eCommerce</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Business</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Entertainment</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Portfolio</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p class="font-medium tracking-wide text-gray-300">Cherry</p>
-            <ul class="mt-2 space-y-2">
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Media</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Brochure</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Nonprofit</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Educational</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Projects</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p class="font-medium tracking-wide text-gray-300">Business</p>
-            <ul class="mt-2 space-y-2">
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Infopreneur</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Personal</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Wiki</a>
-              </li>
-              <li>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Forum</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="md:max-w-md lg:col-span-2">
-          <span class="text-base font-medium tracking-wide text-gray-300">Subscribe for updates</span>
-          <form class="flex flex-col mt-4 md:flex-row">
-            <input
-              placeholder="Email"
-              required=""
-              type="text"
-              class="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-            />
-            <button
-              type="submit"
-              class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Subscribe
-            </button>
-          </form>
-          <p class="mt-4 text-sm text-gray-500">
-            Bacon ipsum dolor amet short ribs pig sausage prosciuto chicken spare ribs salami.
-          </p>
-        </div>
-      </div>
-      <div class="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
-        <p class="text-sm text-gray-500">
-          © Copyright 2020 Lorem Inc. All rights reserved.
+    <div class="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div class="flex flex-col justify-between py-10  border-gray-800 sm:flex-row">
+        <p class="text-sm my-1 text-gray-500">
+          © Copyright 2021 albinvar.in
         </p>
-        <div class="flex items-center mt-4 space-x-4 sm:mt-0">
+        <p class="mx-2 my-1 text-sm text-gray-500">
+          All rights reserved.
+        </p>
+        <div class="my-3 mx-auto flex flex-wrap items-center gap-x-6 justify-between">
           <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400">
             <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
               <path
