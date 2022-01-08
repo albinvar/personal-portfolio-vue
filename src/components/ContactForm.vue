b9a14d14ea67f37deb18f89dbf9c637b5bcfb99d
Albin Varghese <albin8285@gmail.com>
2022-01-08 13:34:03 +0530

    create basic animations

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,8 +2,10 @@
   <div class="flex items-center min-h-screen">
     <div class="container mx-auto">
       <div class="max-w-md mx-auto my-6 bg-white p-5 rounded-md">
-        <div class="m-auto" v-if="emailSendResponse.ok !== null">
-          <div class="bg-white rounded-lg border-gray-300 border p-3 shadow-md" v-if="emailSendResponse.ok">
+        <div class="m-auto mb-8" v-if="emailSendResponse.ok !== null">
+          <div class="bg-white rounded-lg border-gray-300 border p-3 shadow-md mb-3" v-if="emailSendResponse.ok" data-aos="fade-right"
+               data-aos-offset="300"
+               data-aos-easing="ease-in-sine">
             <div class="flex flex-row">
               <div class="px-2">
                 <svg width="24" height="24" viewBox="0 0 1792 1792" fill="#44C997" xmlns="http://www.w3.org/2000/svg">
@@ -31,33 +33,33 @@
             </div>
           </div>
         </div>
-        <div class="text-center mt-12">
-          <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Contact Me</h1>
-          <p class="text-gray-400 dark:text-gray-400">Fill up the form below to send me a message.</p>
+        <div class="text-center mt-4">
+          <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200" data-aos="fade-up">Contact Me</h1>
+          <p class="text-gray-400 dark:text-gray-400" data-aos="fade-up">Fill up the form below to send me a message.</p>
 
         </div>
         <div class="m-7">
           <form method="POST" id="form" @submit.prevent="sendEmail">
-            <div class="mb-6">
+            <div class="mb-6" data-aos="fade-up-right"  data-aos-once="true" >
               <label for="name" class="block mb-2 text-left text-sm text-gray-600 dark:text-gray-400">Full Name</label>
               <input type="text" name="name" id="name" v-model="name" placeholder="John Doe" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
             </div>
-            <div class="mb-6">
+            <div class="mb-6" data-aos="fade-up-left"  data-aos-once="true" >
               <label for="email" class="block mb-2 text-left text-sm text-gray-600 dark:text-gray-400">Email Address</label>
-              <input type="email" name="email" id="email" v-model="email" placeholder="you@company.com" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
+              <input type="email" name="email" id="email" v-model="email" placeholder="you@gmail.com" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
             </div>
-            <div class="mb-6">
+            <div class="mb-6" data-aos="fade-up-right"  data-aos-once="true" >
 
               <label for="phone" class="block mb-2 text-left text-sm text-gray-600 dark:text-gray-400">Phone Number</label>
               <input type="text" name="phone" id="phone" v-model="phone" placeholder="+1 (555) 1234-567" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
             </div>
-            <div class="mb-6">
+            <div class="mb-6" data-aos="fade-up-left"  data-aos-once="true">
               <label for="message" class="block mb-2 text-left text-sm text-gray-600 dark:text-gray-400">Your Message</label>
               <textarea rows="5" id="message" name="message" v-model="message" placeholder="Your Message" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" required></textarea>
             </div>
-            <div class="block mx-auto text-center g-recaptcha my-3 mx-auto" :data-sitekey="siteKey" style="transform:scale(0.89);transform-origin:0 0;"> </div>
-            <div class="mb-6">
-              <input type="submit" class="w-full mx-2 px-3 py-3 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none" value="Send Message">
+            <div class="block mx-auto text-center g-recaptcha my-3 mx-auto" data-aos="fade-up-right"  data-aos-once="true" :data-sitekey="siteKey" style="transform:scale(0.89);transform-origin:0 0;"> </div>
+            <div class="mb-6" data-aos="fade-up"  data-aos-once="true">
+              <input type="submit" @click="scrollToTop()" class="w-full mx-2 px-3 py-3 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none" value="Send Message">
             </div>
             <p class="text-base text-center text-gray-400">
             </p>
@@ -93,6 +95,10 @@ export default {
   },
 
   methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+
     sendEmail(e) {
       try {
         emailjs.sendForm(process.env.VUE_APP_EMAIL_SERVICE_ID, process.env.VUE_APP_EMAIL_TEMPLATE_ID, e.target, process.env.VUE_APP_EMAIL_USER_ID, {
