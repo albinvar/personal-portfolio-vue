0d9d643c3d46305e8488a660488c007f254e76a4
Albin Varghese <albin8285@gmail.com>
2022-01-08 10:12:16 +0530

    create basic views for contact page

---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+{{ formStatus }}
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  title: 'Contact | Albin Varghese',
+  props: [],
+  components: {},
+
+  data() {
+    return {
+      formStatus: true
+    }
+  }
+}
+</script>
