e70a00851d50dae9279e9f9c384202e96b4ebbb4
Albin Varghese <albin8285@gmail.com>
2022-01-08 10:11:22 +0530

    wip:new route for contact page

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,15 @@ const routes = [
     path: '/:catchAll(.*)',
     name: 'NotFound',
     component: NotFound
-  }
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "resume" */ '../views/Contact.vue')
+  },
 ]
 
 const router = createRouter({
